eaee176d6ea18f13f331390dbd4a89d4964385bf
Philipp von Weitershausen <philipp@weitershausen.de>
2007-11-10 10:14:22 +0000

    bump version number after release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name = 'zope.app.apidoc',
-    version = '3.4.3dev',
+    version = '3.4.4dev',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope3-dev@zope.org',
     description = 'API Documentation and Component Inspection for Zope 3',
