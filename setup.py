c5962c9f2fa7ee39bacd7938ff3996fcdddddc21
Dan Korostelev <nadako@gmail.com>
2009-01-17 13:10:51 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name = 'zope.app.apidoc',
-    version = '3.5.0dev',
+    version = '3.5.1dev',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope-dev@zope.org',
     description = 'API Documentation and Component Inspection for Zope 3',
