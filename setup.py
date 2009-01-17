1c507e3858369b93afb04c3e4ce0a9b9c7f9a711
Dan Korostelev <nadako@gmail.com>
2009-01-17 12:38:09 +0000

    Changed cheeseshop to pypi in the url

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope3'],
-    url = 'http://cheeseshop.python.org/pypi/zope.app.apidoc',
+    url = 'http://pypi.python.org/pypi/zope.app.apidoc',
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
