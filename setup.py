afb98a56569b4c050aa25c49891f8cc78e56e5b5
Thomas Lotze <tl@gocept.com>
2009-09-29 15:36:42 +0000

    Back to development: 3.6.8dev

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(
     name = 'zope.app.apidoc',
-    version = '3.6.7',
+    version = '3.6.8dev',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope-dev@zope.org',
     description = 'API Documentation and Component Inspection for Zope 3',
