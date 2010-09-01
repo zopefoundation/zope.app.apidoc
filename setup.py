b125995b68bb441c6357ead3b3044335157e2b60
Gediminas Paulauskas <menesis@pov.lt>
2010-09-01 11:20:38 +0000

    Back to development: 3.7.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(
     name = 'zope.app.apidoc',
-    version='3.7.4',
+    version='3.7.5dev',
     author = 'Zope Corporation and Contributors',
     author_email = 'zope-dev@zope.org',
     description = 'API Documentation and Component Inspection for Zope 3',
